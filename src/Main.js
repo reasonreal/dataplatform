e6433b9ec4b234867e5ffad2db29e943ea535d15
reason <ujini777@naver.com>
2023-09-25 16:12:43 +0900

    CRUD API

--- src/Main.js
+++ src/Main.js
@@ -1,34 +1,54 @@
 import React from "react";
 
 const Main =(props) =>{
-    const getUser = async () => {
-        // GET요청
-        const response = await fetch("/api/v1/table-schema?id=1", { 
-            method: 'GET',
-            headers: {
-                'content-type': 'application/json',
-                "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI2U2JENmd2cEFjMzJMOTZEWjZvYTYwd3FOLVMxbEtaUFZIdU9OT01UaGRrIn0.eyJleHAiOjE2OTUzNzU0NzQsImlhdCI6MTY5NTM3MTg3NCwianRpIjoiODAxZGI2NWUtZmMwYi00OGUyLWFjN2ItMjM3OWNjM2I0ZDdlIiwiaXNzIjoiaHR0cDovLzE5Mi4xNjguMi4xODY6ODA4MC9yZWFsbXMvZGF0YXBsYXRmb3JtX3NlcnZpY2UiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiNGI1MTEwMWItYWQyMy00ODI1LThjNjctZDcyMmY0ZWYzNDI1IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoibWluaW9fY2xpZW50Iiwic2Vzc2lvbl9zdGF0ZSI6IjUyMGVhYzkzLWFiMTAtNGUyMS05MmY0LWQzNjllZDA0NmNmOSIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovLzE5Mi4xNjguMi4xOTM6OTAwMSIsImh0dHA6Ly8xOTIuMTY4LjEuOTI6MTk5OTkiLCJodHRwOi8vMTkyLjE2OC4yLjE5Mzo5MDAwIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsImRlZmF1bHQtcm9sZXMtZGF0YXBsYXRmb3JtX3NlcnZpY2UiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7Im1pbmlvX2NsaWVudCI6eyJyb2xlcyI6WyJhZG1pbiJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIG1pbmlvLWF1dGhvcml6YXRpb24iLCJzaWQiOiI1MjBlYWM5My1hYjEwLTRlMjEtOTJmNC1kMzY5ZWQwNDZjZjkiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicHJlZmVycmVkX3VzZXJuYW1lIjoiZGFlc29vIiwiZ2l2ZW5fbmFtZSI6IiIsImZhbWlseV9uYW1lIjoiIiwiZW1haWwiOiJkYWVzb29AZGF0YW51cmkubmV0IiwicG9saWN5IjpbImNvbnNvbGVBZG1pbiJdfQ.N28DiQrwPlddMhyUmh5OiEDcsrc6B-aCBWQgY8BteR4vHjS1gGNja2S9_60svdjJsBUKnDA4R6jUnnaSiGZNHJgkd-aY_fu4XqnOOeYd2XLlS-UjeU58Qxju8XbHW1Z9i20EdyYXj28nQJAfN7fVjQbceXZHDODqAVgW_Hs-Fx8V9IVmrHLaNp4YQhXlwmxmhq76cB2PpP8vCkI5iowgGd0tMIP39n0WBI50KSqfpcI6SPxI9tXUyMMmwEVK0Oi1EYvKLnTm2BjRX48FyjoARSzo1bT2RQ_4ghI9BJCG1ht0WKnzCId5X100n0s5ER_TIJ_i0vy0ybyM2-gtFZDlhg"            },
-        });
+    // const getUser = async () => {      //GET요청
+    //     const response = await fetch("/api/v1/table-schema?id=1", { 
+    //         method: 'GET',
+    //         headers: {
+    //             'content-type': 'application/json',
+    //             "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI2U2JENmd2cEFjMzJMOTZEWjZvYTYwd3FOLVMxbEtaUFZIdU9OT01UaGRrIn0.eyJleHAiOjE2OTU2MjQzMTIsImlhdCI6MTY5NTYyMDcxMiwianRpIjoiNmJiNzdkZTMtYTY2MS00MTRhLWI3NjgtMDYzOWE0YThhNDMyIiwiaXNzIjoiaHR0cDovLzE5Mi4xNjguMi4xODY6ODA4MC9yZWFsbXMvZGF0YXBsYXRmb3JtX3NlcnZpY2UiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiNGI1MTEwMWItYWQyMy00ODI1LThjNjctZDcyMmY0ZWYzNDI1IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoibWluaW9fY2xpZW50Iiwic2Vzc2lvbl9zdGF0ZSI6ImRhMzZkM2VmLWM4N2UtNDNkYy1iYTI1LTk5NTEzOGYyODU0MiIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovLzE5Mi4xNjguMi4xOTM6OTAwMSIsImh0dHA6Ly8xOTIuMTY4LjEuOTI6MTk5OTkiLCJodHRwOi8vMTkyLjE2OC4yLjE5Mzo5MDAwIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsImRlZmF1bHQtcm9sZXMtZGF0YXBsYXRmb3JtX3NlcnZpY2UiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7Im1pbmlvX2NsaWVudCI6eyJyb2xlcyI6WyJhZG1pbiJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIG1pbmlvLWF1dGhvcml6YXRpb24iLCJzaWQiOiJkYTM2ZDNlZi1jODdlLTQzZGMtYmEyNS05OTUxMzhmMjg1NDIiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicHJlZmVycmVkX3VzZXJuYW1lIjoiZGFlc29vIiwiZ2l2ZW5fbmFtZSI6IiIsImZhbWlseV9uYW1lIjoiIiwiZW1haWwiOiJkYWVzb29AZGF0YW51cmkubmV0IiwicG9saWN5IjpbImNvbnNvbGVBZG1pbiJdfQ.XdT5hIZricgtzupIZNPpa9wUjdtwBWZg8k75lVLRlzF-ARtiT8tByETu3-_fWswbedM0yVSMqjhbSC4c6tIXB3SSr2ElQXQ90MXqQ0sce1vAZewk6O_60a9AONi2teJgYWTWEvySguqhDoaRxGlFloJ4lWPJWpb2xmCMadKC-Oam3Rv5AW6OvWGHl_dKXljTXBQcnfOI2JZPGR9TNCR-qtHiBqHYHubRVVpSNUHWTJm8pNZsFQ9Dfg-iIZIn4yQ_9__X4A3Lsi82mzpRwDiX-Hcs4IH3jhl7IIfLRYKv-7OPxo0D-cBwot-OnJFv2yJ9Sp2Cqndqi_7HGhIogcVR5Q"           
+    //            },
+    //     });
     
-        if (response.status === 200) {
-          const data = await response.json();
-          console.log(data);
-        } else {
-          const error = await response.json();
-          console.log(error);
-        }
-    }
+    //     if (response.status === 200) {
+    //       const data = await response.json();
+    //       console.log(data);
+    //     } else {
+    //       const error = await response.json();
+    //       console.log(error);
+    //     }
+    // }
   
-    // // delete 
-    // const deleteSchema = async (id) => {
+
+     
+    const registUser = async (schema) => {       //Post 요청
+      const response = await fetch("/api/v1/table-schema", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI2U2JENmd2cEFjMzJMOTZEWjZvYTYwd3FOLVMxbEtaUFZIdU9OT01UaGRrIn0.eyJleHAiOjE2OTU2MjcyMzksImlhdCI6MTY5NTYyMzYzOSwianRpIjoiNzBlNTVkNWItYTg2NS00MTcxLWFmMTUtZjQ1YTBkYTlmYWZiIiwiaXNzIjoiaHR0cDovLzE5Mi4xNjguMi4xODY6ODA4MC9yZWFsbXMvZGF0YXBsYXRmb3JtX3NlcnZpY2UiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiNGI1MTEwMWItYWQyMy00ODI1LThjNjctZDcyMmY0ZWYzNDI1IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoibWluaW9fY2xpZW50Iiwic2Vzc2lvbl9zdGF0ZSI6ImNhYjk1M2Q0LTQzNTAtNDc5MS1hOTU4LWY5MzFkOGUxN2M5NyIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovLzE5Mi4xNjguMi4xOTM6OTAwMSIsImh0dHA6Ly8xOTIuMTY4LjEuOTI6MTk5OTkiLCJodHRwOi8vMTkyLjE2OC4yLjE5Mzo5MDAwIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsImRlZmF1bHQtcm9sZXMtZGF0YXBsYXRmb3JtX3NlcnZpY2UiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7Im1pbmlvX2NsaWVudCI6eyJyb2xlcyI6WyJhZG1pbiJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIG1pbmlvLWF1dGhvcml6YXRpb24iLCJzaWQiOiJjYWI5NTNkNC00MzUwLTQ3OTEtYTk1OC1mOTMxZDhlMTdjOTciLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicHJlZmVycmVkX3VzZXJuYW1lIjoiZGFlc29vIiwiZ2l2ZW5fbmFtZSI6IiIsImZhbWlseV9uYW1lIjoiIiwiZW1haWwiOiJkYWVzb29AZGF0YW51cmkubmV0IiwicG9saWN5IjpbImNvbnNvbGVBZG1pbiJdfQ.HV0uXlDB35qwM4V8pQygXb4PqeomFrYib49mzB7n3XhzMpyEyn58E-LLx9ZbauOvK0qrikHOREXxHXiDOBkZ3zxycao-lMMkZfeO4JMlIVK9OT3tXCP5c8O4YIKwn1HUJLTxtd8RE7_xkBUGOZf6w3QsjFyWVD5t9Jf8tf9ZdJjc6h3Ml0BO16EjAMB9wc5hozXLzhJi6IW3mW3nL6SGuAL_vkkvDUYjUVW8b4Y4CRPHEJoTfwr5vozO8hiKYQQAdvz2p9ltkrhQk_UBsRuxuxJKTbwD6TnX8b4p0Djh4Yv6Fhbb2C2Mg0Zs9CudLG2MiqFr8YgBY0U_QMFb2ucHRg"
+        },
+        body: schema,
+      });
+  
+      if (response.status === 201) {
+        console.log("Schema created successfully!");
+      } else {
+        console.log("Failed to create schema.");
+      }
+    };
+
+    
+    
+    // const removeUser = async (id) => {        // delete 
     //     const response = await fetch("/api/v1/table-schema?id=1", {
     //       method: "DELETE",
     //       headers: {
     //         "Content-Type": "application/json",
-    //         "Authorization": "Bearer your-token",
+    //         "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI2U2JENmd2cEFjMzJMOTZEWjZvYTYwd3FOLVMxbEtaUFZIdU9OT01UaGRrIn0.eyJleHAiOjE2OTU2MjMwMjEsImlhdCI6MTY5NTYxOTQyMSwianRpIjoiYTdhNGI0NzQtZDg0Zi00YWRlLWJhNzMtYWI2YjNkODczZGI1IiwiaXNzIjoiaHR0cDovLzE5Mi4xNjguMi4xODY6ODA4MC9yZWFsbXMvZGF0YXBsYXRmb3JtX3NlcnZpY2UiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiNGI1MTEwMWItYWQyMy00ODI1LThjNjctZDcyMmY0ZWYzNDI1IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoibWluaW9fY2xpZW50Iiwic2Vzc2lvbl9zdGF0ZSI6IjRjMWE0NDYyLTRlMDQtNGY4Mi1hMjgyLTA2NGZkMTZkN2ZkMiIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovLzE5Mi4xNjguMi4xOTM6OTAwMSIsImh0dHA6Ly8xOTIuMTY4LjEuOTI6MTk5OTkiLCJodHRwOi8vMTkyLjE2OC4yLjE5Mzo5MDAwIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsImRlZmF1bHQtcm9sZXMtZGF0YXBsYXRmb3JtX3NlcnZpY2UiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7Im1pbmlvX2NsaWVudCI6eyJyb2xlcyI6WyJhZG1pbiJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIG1pbmlvLWF1dGhvcml6YXRpb24iLCJzaWQiOiI0YzFhNDQ2Mi00ZTA0LTRmODItYTI4Mi0wNjRmZDE2ZDdmZDIiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicHJlZmVycmVkX3VzZXJuYW1lIjoiZGFlc29vIiwiZ2l2ZW5fbmFtZSI6IiIsImZhbWlseV9uYW1lIjoiIiwiZW1haWwiOiJkYWVzb29AZGF0YW51cmkubmV0IiwicG9saWN5IjpbImNvbnNvbGVBZG1pbiJdfQ.dqNeffxDEz7i-jJezEryj6Y6Nf1gzOV0OPDT2Kec_LSWg5lFKsM8zaMOHpXZ8qeplQnl18r5TSiy55bnBLmxeUoFOj84ekdKrN2y4WeGgbmzqCTyx9BYtsV7GVcD3fxruAB7dXaTZsnbAtdhWJr4h3xB4y2Qpk-d5iyFIzhNVUOK_cBPLaKlKZrnVw_EmFsS58wo4mBychGrq0qwz4h7vtaaL5DDdZOVmmnbYy9aYz82yE6quQ_AmURuKh8HPQgH5cgmG2CzUC09MY3h_624ccPiMQu5RjkxVtG6qdhP5BxPqqm_W4GVNW9t_T7i9XKPuT5JdqctcT3Gu55p6HUCyw",
     //       },
     //       query: {
-    //         id: id,
+    //         id: 1,
     //       },
     //     });
       
@@ -40,33 +60,13 @@ const Main =(props) =>{
     //   };
 
 
-
-    //   // Post 요청 
-    // const postSchema = async (schema) => {
-    //   const response = await fetch("/api/v1/table-schema", {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI2U2JENmd2cEFjMzJMOTZEWjZvYTYwd3FOLVMxbEtaUFZIdU9OT01UaGRrIn0.eyJleHAiOjE2OTUzNzEyMTUsImlhdCI6MTY5NTM2NzYxNSwianRpIjoiNTExZjExZGQtMjk3NS00ZTU5LWI4MTItZjRjM2Q0OGNlNmZiIiwiaXNzIjoiaHR0cDovLzE5Mi4xNjguMi4xODY6ODA4MC9yZWFsbXMvZGF0YXBsYXRmb3JtX3NlcnZpY2UiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiNGI1MTEwMWItYWQyMy00ODI1LThjNjctZDcyMmY0ZWYzNDI1IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoibWluaW9fY2xpZW50Iiwic2Vzc2lvbl9zdGF0ZSI6IjViMDM4YmNiLTQ5MGMtNDUyMS1iNzZiLTJkNmUzZjNkMDNmNCIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovLzE5Mi4xNjguMi4xOTM6OTAwMSIsImh0dHA6Ly8xOTIuMTY4LjEuOTI6MTk5OTkiLCJodHRwOi8vMTkyLjE2OC4yLjE5Mzo5MDAwIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsImRlZmF1bHQtcm9sZXMtZGF0YXBsYXRmb3JtX3NlcnZpY2UiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7Im1pbmlvX2NsaWVudCI6eyJyb2xlcyI6WyJhZG1pbiJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIG1pbmlvLWF1dGhvcml6YXRpb24iLCJzaWQiOiI1YjAzOGJjYi00OTBjLTQ1MjEtYjc2Yi0yZDZlM2YzZDAzZjQiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicHJlZmVycmVkX3VzZXJuYW1lIjoiZGFlc29vIiwiZ2l2ZW5fbmFtZSI6IiIsImZhbWlseV9uYW1lIjoiIiwiZW1haWwiOiJkYWVzb29AZGF0YW51cmkubmV0IiwicG9saWN5IjpbImNvbnNvbGVBZG1pbiJdfQ.C1H3uZQ11VxSDGMfiOUdmtuC0MvA9_emgJRr2zYzcdxSw5UAMrTpyapDZtuxH9kIdoX2jdb5m0aaSPcZuN1iXjK4zaZYoCwvRKJNfwPZxuMvEsTY8gR_FCa_3dFba1FP9bfFgfgXDU3yAAAo6DMTe8PjMmLDMXkKEW92Mv62zAHsseatnFjFkOlA-YzpO4Bt36aNgbUgnhu0WULQvOm38ZY9kQXDohNkRmH375STlpZWIk6xsW23th6gfZA9nwpsWGRWdsSjXd8RCfHJUo47gK35U5kwaOsX2sKYv5v80U59WPLBYoAqncr15RMhQBetXsN80fqL5vzYqp9FjzHmGQ"
-    //     },
-    //     body: JSON.stringify(schema),
-    //   });
-  
-    //   if (response.status === 201) {
-    //     console.log("Schema created successfully!");
-    //   } else {
-    //     console.log("Failed to create schema.");
-    //   }
-    // };
-  
-
-    //put 요청
-    // const putSchema = async (schema) => {
+    
+    // const modifyUser = async (schema) => {      //put 요청
     //     const response = await fetch("/api/v1/table-schema", {
     //       method: "PUT",
     //       headers: {
     //         "Content-Type": "application/json",
-    //         "Authorization": "Bearer your-token",
+    //         "Authorization": " ",
     //       },
     //       body: JSON.stringify(schema),
     //     });
@@ -79,26 +79,13 @@ const Main =(props) =>{
     // };
     
 
- 
-    // const handleCreate = () => {     //post 요청
-    //   postSchema(schema);
-    // };
-  
-    // const handleDelete = (id) => {      //delete 요청
-    //     deleteSchema(id);
-    //   };
-
-    // const putSchema = (id) => {      //put 요청
-    //   deleteSchema(id);
-    // };
-    
-  
+       
     return(
     <>
-        <button onClick = {getUser}>get</button>
-        {/* <button onClick = {postSchema}>Create</button> */}
-        {/* <button onClick= {deleteSchema}>delete</button>
-        <button onClick = {putSchema}>put</button> */}
+        {/* <button onClick = {getUser}>get</button> */}
+        <button onClick = {registUser}>Create</button>
+        {/* <button onClick= {removeUser}>delete</button> */}
+        {/* <button onClick = {modifyUser}>put</button> */}
     </>
     );
 };
